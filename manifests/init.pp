0798ae23087fbd67062700fed126574c862f5fc4
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-01-10 14:03:29 +0100

    Lint nginx::init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,20 @@
 class nginx {
   include nginx::params
 
-  package { 'nginx': 
+  package { 'nginx':
     ensure => installed,
   }
 
   service { 'nginx':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
     require => [ Package['nginx'], File['/etc/nginx/nginx.conf'] ],
   }
 
-  exec {"nginx-reload":
+  exec {'nginx-reload':
     refreshonly => true,
-    command     => "nginx -s reload",
-    onlyif      => "nginx -t",
+    command     => 'nginx -s reload',
+    onlyif      => 'nginx -t',
   }
 
   $nginx_user = $nginx::params::nginx_user
@@ -26,13 +26,14 @@ class nginx {
 
   file { '/usr/local/bin/fcgi-wrapcgi.pl':
     source => 'puppet:///modules/nginx/fcgi-wrapcgi.pl',
-    mode   => 0755,
+    mode   => '0755',
     owner  => 'root',
     group  => 'root',
     before => Service['nginx']
   }
 
-  file { ["/etc/nginx/sites-available", "/etc/nginx/sites-enabled", "/etc/nginx/conf.d"]:
+  file { ['/etc/nginx/sites-available',
+  '/etc/nginx/sites-enabled', '/etc/nginx/conf.d',]:
     ensure  => directory,
     require => Package['nginx'],
     notify  => Service['nginx'],
